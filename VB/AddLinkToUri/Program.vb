74a921f53a7e3d87078774b86dddf481de3d3f91
DevExpressExampleBot <fakeEmail>
2022-02-02 12:16:49 +0300

    Source auto update [skip ci]

--- VB/AddLinkToUri/Program.vb
+++ VB/AddLinkToUri/Program.vb
@@ -1,23 +1,20 @@
-﻿Imports System
+Imports System
 Imports DevExpress.Pdf
 Imports System.Drawing
 
 Namespace AddLinkToUri
+
     Friend Class Program
-        Shared Sub Main(ByVal args() As String)
 
-            Using processor As New PdfDocumentProcessor()
-
+        Shared Sub Main(ByVal args As String())
+            Using processor As PdfDocumentProcessor = New PdfDocumentProcessor()
                 ' Create an empty document.
                 processor.CreateEmptyDocument("..\..\Result.pdf")
-
                 ' Create and draw graphics.
                 Using graphics As PdfGraphics = processor.CreateGraphics()
                     DrawGraphics(graphics)
-
                     ' Create a link to URI specifying link area and URI.
                     graphics.AddLinkToUri(New RectangleF(310, 150, 180, 15), New Uri("https://www.devexpress.com"))
-
                     ' Render a page with graphics.
                     processor.RenderNewPage(PdfPaperSize.Letter, graphics)
                 End Using
@@ -25,9 +22,8 @@ Namespace AddLinkToUri
         End Sub
 
         Private Shared Sub DrawGraphics(ByVal graphics As PdfGraphics)
-
             ' Draw a text line on the page. 
-            Using font As New Font("Arial", 10)
+            Using font As Font = New Font("Arial", 10)
                 Dim blue As SolidBrush = CType(Brushes.Blue, SolidBrush)
                 graphics.DrawString("https://www.devexpress.com", font, blue, 310, 150)
             End Using
